df4eb04eec78e4f01cb5099f64c3d6ef4187cbed
he kai <648851130@qq.com>
2017-03-03 11:38:46 +0800

    vue-5实现，非原创，css3旋转动画修改

--- 24/app.js
+++ 24/app.js
@@ -1,118 +1,219 @@
-;(function(window){
+;(function(window,document){
+let fragment, currentNodeList = [];
+
+function Vue(options){
+
+  this._init(options);
+}
+
+Vue.prototype = {
+    constructor: Vue,
+    _init,
+    observer: Observer,
+    $watch,
+    _compile:function(){
+      fragment = document.createDocumentFragment();
+      currentNodeList.push(fragment);
+
+      this._compileNode(this.$template);
+
+     this.$el.parentNode.replaceChild(fragment, this.$el);
+     console.log(fragment)
+
+     this.$el = document.querySelector(this.$options.el);
+
+    },
+    _compileNode:function(node){
+      console.log(node)
+      switch(node.nodeType){
+        // text
+        case 1:
+            this._compileElement(node);
+            break;
+        // node
+        case 3 :
+            this._compileText(node);
+            break;
+        default:
+            return;
+
+      }
+    },
+    _compileElement:function(node){
+      let newNode = document.createElement(node.tagName);
+
+    // 处理节点属性
+    if (node.hasAttributes()) {
+        let attrs = node.attributes;
+        Array.from(attrs).forEach((attr) => {
+            newNode.setAttribute(attr.name, attr.value);
+        });
+    }
 
 
-  // 观察者构造函数
+    let currentNode = currentNodeList[currentNodeList.length - 1].appendChild(newNode);
+
+    if (node.hasChildNodes()) {
+        currentNodeList.push(currentNode);
+        Array.from(node.childNodes).forEach(this._compileNode, this);
+    }
+
+    currentNodeList.pop();
+
+  },
+  _compileText: function (node) {
+    // console.log(node)
+      let nodeValue = node.nodeValue;
+
+      if (nodeValue === '') return;
+
+      let patt = /{{([^}}]+)?}}/gi;
+      let ret = nodeValue.match(patt);
+      let d = this.$data;
+
+      // console.log(ret)
+
+      if (!ret) return;
+
+      ret[0].replace(/[{}]/g, '').split('.').forEach((value) => {
+          d = d[value];
+      }, this);
+
+      nodeValue = nodeValue.replace(ret[0],d);
+
+      // console.log(currentNodeList)
+      currentNodeList[currentNodeList.length - 1].appendChild(document.createTextNode(nodeValue));
+  }
+
+};
+
+function _init(options){
+  this.$options = options;
+  this.$data = options.data;
+  this.$el = document.querySelector(options.el);
+  this.$template = this.$el.cloneNode(true);
+
+  this.observer = new this.observer(this.$data);
+  this.observer.on('set', this._compile.bind(this));
+
+  this._compile();
+}
+
+function $watch(key,fn){
+  let _fn = function () {
+      fn(arguments[2]);
+  };
+
+  let pathAry = key.split('.');
+  if (pathAry.length === 1) {
+      this.$data.$observer.on(`set:${key}`, _fn.bind(this));
+  } else {
+      let _temp = this.$data;
+      let lastProperty = pathAry.pop();
+      pathAry.forEach((property) => {
+          _temp = _temp[property]; //a.b -> b.c -> c.d
+      });
+      _temp.$observer.on(`set:${lastProperty}`, _fn.bind(this));
+  }
+}
+
+// 观察者构造函数
 function Observer(data) {
-  var args = Array.prototype.slice.call(arguments);
-    this.data = data.data;
+  this.data = data;
 
-    // if(args[1] && args[1].watch){
-    //   this.$watch(args[1].name,args[1].cb);
-    // } else {
-    //   this.walk(data);
-    // }
-    //
+  Object.defineProperty(data, '$observer', {
+          value: this,
+          enumerable: false,
+          writable: true,
+          configurable: true
+      });
 
-    var elem = document.querySelectorAll(data.el)[0];
-    var template = elem.innerHTML;
-    template = template.replace(/[\r\t\n\s]/g, '');
-
-
-   elem.innerHTML = this.templateEngine(template,this.data);
+  this.walk(data);
 }
 
 Observer.prototype = {
-  constructor:Observer,
-  templateEngine: function(html, options){
-    var re = /\{\{([^\}\}]+)?\}\}/gi, reExp = /(^( )?(if|for|else|switch|case|break|{|}))(.*)?/g, code = 'var r=[];\n', cursor = 0;
-    var add = function(line, js) {
-      js ? (code += line.match(reExp) ? line + '\n' : 'r.push(data.' + line + ');\n') :
-        (code += line !== '' ? 'r.push("' + line.replace(/"/g, '\\"') + '");\n' : '');
-      return add;
-    };
-    while (match = re.exec(html)) {
-      add(html.slice(cursor, match.index))(match[1], true);
-      cursor = match.index + match[0].length;
-    }
-    add(html.substr(cursor, html.length - cursor));
-    code += 'return r.join("");';
-    console.log(code.replace(/[\r\t\n]/g, ''));
-    return new Function(code.replace(/[\r\t\n]/g, '')).apply(options);
-  },
-  walk: function (obj) {
-      var val;
-      for (var key in obj) {
-          // 这里为什么要用hasOwnProperty进行过滤呢？
-          // 因为for...in 循环会把对象原型链上的所有可枚举属性都循环出来
-          // 而我们想要的仅仅是这个对象本身拥有的属性，所以要这么做。
-          if (obj.hasOwnProperty(key)) {
-              val = obj[key];
+  constructor: Observer,
+  walk:function (obj) {
+    var val;
 
-              // 这里进行判断，如果还没有遍历到最底层，继续new Observer
-              if (typeof val === 'object') {
-                  new Observer(val);
-              }
-
-              this.convert(key, val);
-          }
-      }
-  },
-  convert: function (key, val, cb) {
-      Object.defineProperty(this.data, key, {
-          enumerable: true,
-          configurable: true,
-          get: function () {
-              // console.log('你访问了' + key);
-              return val;
-          },
-          set: function (newVal) {
-
-              if (newVal === val) return;
-              if(typeof newVal === 'object'){
-                new Observer(newVal);
-              }
-
-              val = newVal;
-
-              if(!!cb){
-                cb(newVal);
-              }else{
-                console.log('你设置了' + key);
-                console.log('新的' + key + ' = ' + newVal);
-              }
-          }
-      });
-  },
-  $watch: function(name,cb) {
-    var obj = this.data,val,
-    names = name.split('.');
     for (var key in obj) {
-
+        // 这里为什么要用hasOwnProperty进行过滤呢？
+        // 因为for...in 循环会把对象原型链上的所有可枚举属性都循环出来
+        // 而我们想要的仅仅是这个对象本身拥有的属性，所以要这么做。
         if (obj.hasOwnProperty(key)) {
             val = obj[key];
-            if(key === names[0]){
 
-              if(names.length === 1){
-                this.convert(key, val, cb);
-                if(typeof val === 'object'){
-                  for(var k in val){
-                    new Observer(val,{watch:true,name:k,cb:cb});
-                  }
-                }
-              } else {
-                if (typeof val === 'object') {
-                  new Observer(val,{watch:true,name:names.slice(1).join('.'),cb:cb});
-                }
-              }
+            // 这里进行判断，如果还没有遍历到最底层，继续new Observer
+            if (typeof val === 'object') {
+                this.observe(key,val);
             }
 
+            this.convert(key, val);
         }
     }
   },
+  convert:function (key, val) {
+
+    let ob = this;
+    Object.defineProperty(this.data, key, {
+        enumerable: true,
+        configurable: true,
+        get: function () {
+            console.log('你访问了' + key);
+            return val;
+        },
+        set: function (newVal) {
+
+            if (newVal === val) return;
+
+            val = newVal;
+            console.log('你设置了' + key);
+            console.log('新的' + key + ' = ' + newVal);
+            ob.notify('set', key, newVal);
+            ob.notify(`set:${key}`, key, newVal);
+        }
+    });
+  },
+  observe :function (key, val) {
+    let ob = new Observer(val);
+    if (!ob) return;
+    ob.parent = {
+        key,
+        ob: this
+    };
+  },
+  on: function(event,fn) {
+    this._cbs = this._cbs || {};
+    if (!this._cbs[event]) {
+        this._cbs[event] = [];
+    }
+    this._cbs[event].push(fn);
+
+    // 这里return this是为了实现.on(...).on(...)这样的级联调用
+    return this;
+  },
+  emit: function (event, path, val) {
+      this._cbs = this._cbs || {};
+      let callbacks = this._cbs[event];
+      if (!callbacks) return;
+      callbacks = callbacks.slice(0);
+      callbacks.forEach((cb, i) => {
+          callbacks[i].apply(this, arguments);
+      });
+  },
+  notify :function (event, path, val) {
+      this.emit(event, path, val);
+      let parent = this.parent;
+      if (!parent) return;
+      let ob = parent.ob;
+      ob.notify(event, path, val);
+  }
 
 };
 
 
 
-  window.Observer = Observer;
-})(window);
+window.Vue = Vue;
+
+
+})(window,document);
